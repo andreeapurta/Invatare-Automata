4d1b097dc496d1f8a3c0474b9fee9f2c47d2faf5
andreeapurta <andreea.purta@gmail.com>
2021-10-26 11:54:08 +0300

    solution fixed

---
+++ Algoritmul K-Means/Centroid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KMeansAlgorithm
+{
+    public class Centroid
+    {
+        public string Name { get; set; }
+        public Point Center { get; set; }
+        public List<Point> AssignedPoints { get; set; }
+
+        public Centroid(int centerX, int centerY, string name)
+        {
+            if (centerX > 300 || centerX < -300)
+            {
+                throw new ArgumentOutOfRangeException(nameof(centerX), "All coordinates have to be in [-300, 300] interval");
+            }
+            if (centerY > 300 || centerY < -300)
+            {
+                throw new ArgumentOutOfRangeException(nameof(centerY), "All coordinates have to be in [-300, 300] interval");
+            }
+
+            Center = new Point(centerX, centerY);
+            Name = name;
+            AssignedPoints = new List<Point>();
+        }
+    }
+}
